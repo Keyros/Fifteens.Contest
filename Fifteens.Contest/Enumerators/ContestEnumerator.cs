df105be804cdbb2e02b082d4f3256902a701e16b
Rasim Keita <keita.rasim@gmail.com>
2022-09-18 03:13:45 +0400

    * code fix

--- Fifteens.Contest/Enumerators/ContestEnumerator.cs
+++ Fifteens.Contest/Enumerators/ContestEnumerator.cs
@@ -1,22 +1,30 @@
-namespace Fifteens.Contest;
+namespace Fifteens.Contest.Enumerators;
 
-public sealed class SimpleContestEnumerator : IAsyncEnumerator<int>, IAsyncEnumerable<int>
+public sealed class ContestEnumerator : IAsyncEnumerator<int>, IAsyncEnumerable<int>
 {
     private readonly int _finish;
+    private readonly Random _rnd;
 
-    public SimpleContestEnumerator((int start, int end) input)
+    public ContestEnumerator((int start, int end) input)
     {
         _finish = input.end;
         Current = input.start - 1;
+        _rnd = new Random();
     }
-
-
+    
     public ValueTask DisposeAsync() => ValueTask.CompletedTask;
 
     public async ValueTask<bool> MoveNextAsync()
     {
         await Task.Yield();
+        int number;
+        do
+        {
+            number = _rnd.Next();
+        } while (number != Current);
+
         Current++;
+
         return Current < _finish + 1;
     }
 
--- Fifteens.Contest/Program.cs
+++ Fifteens.Contest/Program.cs
@@ -1,6 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using Fifteens.Contest;
+﻿using Fifteens.Contest;
+using Fifteens.Contest.Enumerators;
 
 Console.WriteLine("Hello, World!");
 
@@ -16,10 +15,6 @@ var inputProcessors = Enumerable.Range(1, countToOutput)
 var tasks = inputProcessors.Select(x => x.ProcessData());
 
 Console.WriteLine(string.Join(',', await Task.WhenAll(tasks)));
-
-
-IAsyncEnumerable<int> CreateSimpleEnumerator((int start, int end) input) =>
-    Enumerable.Range(input.start, input.end - input.start + 1).ToAsyncEnumerable();
 
 Console.WriteLine("Completed.");
 Console.ReadLine();
